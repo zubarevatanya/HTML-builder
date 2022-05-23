2266003e9e5c4cf8af7bedbfc5086df99b104bd9
Zakharchenko Tanya <zubarevatanya@gmail.com>
2022-05-23 23:06:35 +0400

    feat: implement task 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,23 @@
+import * as fs from 'fs';
+import * as path from 'path';
 
+
+const copyDir = (sourceDir, targetDir) => {
+    fs.readdir(sourceDir, { withFileTypes: true }, (err, files) => {
+        files.forEach((f) => {
+            if (f.isDirectory()) {
+                copyDir(path.join(sourceDir, f.name), path.join(targetDir, f.name))
+            }
+            else {
+                fs.mkdir(targetDir, { recursive: true }, (err) => {
+                    if (err) throw err;
+                    fs.copyFile(path.join(sourceDir, f.name), path.join(targetDir, f.name),(err) => {
+                        if (err) throw err;
+                    })
+                });
+            }
+        })
+    })
+}
+
+copyDir("./04-copy-directory/files", "./04-copy-directory/copy-files")
